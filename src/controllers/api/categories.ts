725812764126cdee7bd8fa81856059a91cf0f4d5
Mohamed <KEITA>
2017-12-13 00:10:32 +0100

    create api categories controler

--- src/controllers/api/categories.ts
+++ src/controllers/api/categories.ts
@@ -2,3 +2,41 @@
 import { Response, Request, NextFunction } from 'express';
 import { default as Categories, CategoriesModel } from '../../models/categories.model';
 const utils = require('../../helpers/utils');
+
+export = {
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     */
+    find: (req: Request, res: Response, next: NextFunction) => {
+    },
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     */
+    create: (req: Request, res: Response, next: NextFunction) => {
+    },
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     */
+    put: (req: Request, res: Response, next: NextFunction) => {
+    },
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     */
+    delete: (req: Request, res: Response, next: NextFunction) => {
+    },
+};
