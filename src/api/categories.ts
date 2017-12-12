d06fbc988bc1040518b17951fd755fab5fef6df9
Mohamed <KEITA>
2017-12-13 00:45:28 +0100

    added create categories and find process

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -1,6 +1,6 @@
 'use strict';
 import { Response, Request, NextFunction } from 'express';
-import { default as Categories, CategoriesModel } from '../../models/categories.model';
+import { default as Categories, CategoriesModel } from '../models/categories.model';
 const utils = require('../../helpers/utils');
 
 export = {
@@ -11,6 +11,9 @@ export = {
      * @param next
      */
     find: (req: Request, res: Response, next: NextFunction) => {
+        Categories.find().then((categories) => {
+            res.status(200).json(categories);
+        }).catch((next));
     },
 
     /**
@@ -20,6 +23,21 @@ export = {
      * @param next
      */
     create: (req: Request, res: Response, next: NextFunction) => {
+        const categories = new Categories({
+            name: req.body.name,
+            img: req.body.img || undefined
+        });
+        Categories.findOne({name: req.body.name}).then((categories) => {
+            if (categories === null) {
+                categories.save().then(savedObj => {
+                    res.status(201).json(savedObj);
+                }).catch((err) => {
+                    console.log(err);
+                });
+            } else {
+                res.status(409).json({code: 'existingCategories', message: 'categories already exist'});
+            }
+        }).catch((next));
     },
 
     /**
